069b58555b55e38be4fda35e123d910a91aab8c5
ThienKhang410 <khangdt410@gmail.com>
2025-06-09 22:33:59 +0700

    Init project 3 - movie app Flixx

---
+++ project3-mvie-app-flixx/js/script.js
@@ -0,0 +1,37 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+function highlightMenu() {
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
+// Init App
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      console.log("Home");
+      break;
+    case "/shows.html":
+      console.log("Shows");
+      break;
+    case "/movie-details.html":
+      console.log("Movie Details");
+      break;
+    case "/tv-details.html":
+      console.log("TV Details");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+  }
+
+  highlightMenu();
+}
+window.addEventListener('DOMCotentLoaded', init());
